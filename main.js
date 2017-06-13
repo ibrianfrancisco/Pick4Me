c6b294963a3585a96f9b424e9d7ac872ff99b83a
Brian Francisco <ibrianfrancisco@gmail.com>
2017-06-13 00:38:22 -0700

    set return statements

--- main.js
+++ main.js
@@ -3,23 +3,33 @@ const input = document.querySelector('input');
 const form = document.querySelector('form');
 const button = document.querySelector('button');
 const choice = document.querySelector('.choice');
+let trigger = false;
 let choices = [];
 
 function chooseOne(e) {
-  let rdm = Math.random();
-  let idx = Math.floor(rdm * choices.length);
+  let idx = Math.floor(Math.random() * choices.length);
+  if (!trigger) return choice.innerHTML = 'Enter some choices!';
   choice.innerHTML = `${choices[idx]} sounds good!`;
 }
 
 function addToList(e) {
   e.preventDefault();
-  elements = document.querySelectorAll('li');
+  if (input.value === '') return;
+  trigger = true;
   choices.push(input.value);
   list.innerHTML =
     choices
       .map(choice => `<li>${choice}</li>`)
       .join('');
+  choice.innerHTML = '';
   input.value = '';
+}
+
+function reset() {
+  list.innerHTML = '';
+  choice.innerHTML = '';
+  choices = [];
+  trigger = false;
 }
 
 form.addEventListener('submit', addToList);
